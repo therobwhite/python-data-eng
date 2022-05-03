8929439f7f49c2af5f1758d359c12daf036133fe
Rob White <therobwhite@hotmail.co.uk>
2022-05-04 00:00:24 +0100

    First set of tests to assert sorted tuple

--- app/nypd_analysis.py
+++ app/nypd_analysis.py
@@ -13,9 +13,31 @@ def read_csv(filename):
     return new_list
 
 
+def sort_ofns_desc(nypd_list):
+    ofns_dict = {}
+    for row_dict in nypd_list:
+        key = row_dict["OFNS_DESC"]
+        if key in ofns_dict.keys():
+            count =  ofns_dict[key]
+            count += 1 
+            ofns_dict.update({key: count})
+        # If this exists as a key in OFNS_DESC Dictionary then add to value or create key
+        else:
+            ofns_dict[key] = 1
+
+    sorted_tuple  = sorted(ofns_dict.items(), key=lambda x:x[1], reverse=True)
+
+    return sorted_tuple
+
+if __name__ == "__main__" :       
+    nypd_list = read_csv(filename)
+    ofns_sorted_tuple = sort_ofns_desc(nypd_list)
+
+    print(ofns_sorted_tuple)
+    # print(nypd_list)
+    # print(ofns_dict)
 
 
 
-nypd_list = read_csv(filename)
 
             
